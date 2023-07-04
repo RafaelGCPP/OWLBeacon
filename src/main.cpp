993bfbab0186fda3b769d2948503180f8995c0ad
Rafael Pinto <rafaelgcpp@gmail.com>
2023-07-04 16:27:56 -0300

    Seting up the display

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include "tft_config.h"
+#include <TFT_eSPI.h> 
+#include <SPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+TFT_eSPI tft = TFT_eSPI(); 
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+
+    pinMode(PIN_POWER_ON, OUTPUT);
+    digitalWrite(PIN_POWER_ON, HIGH);
+
+    tft.init();
+    tft.setRotation(1);
+    tft.fillScreen(TFT_BLACK);
+
+
+    tft.fillScreen(TFT_RED); delay(1000);
+    tft.fillScreen(TFT_GREEN); delay(1000);
+    tft.fillScreen(TFT_BLUE); delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
